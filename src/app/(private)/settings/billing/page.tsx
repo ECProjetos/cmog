d0461170f1caaf1e8d624d1ea145904d48c64806
joaole <joaole64@gmail.com>
2025-07-14 15:37:45 -0300

    fix: correct user ID field in subscription query and improve logging

--- src/app/(private)/settings/billing/page.tsx
+++ src/app/(private)/settings/billing/page.tsx
@@ -3,6 +3,21 @@
 import { Button } from "@/components/ui/button";
 import { useUserStore } from "@/stores/userStore";
 import { toast } from "sonner";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import Link from "next/link";
 
 export default function BillingPage() {
   const user = useUserStore((state) => state.user);
@@ -26,13 +41,38 @@ export default function BillingPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
-        <h1 className="text-2xl font-bold text-center text-gray-900 dark:text-white">
-          Gerenciar Assinatura
-        </h1>
-        <Button onClick={handleManageSubscription}>Gerenciar Assinatura</Button>
-      </div>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <div className="flex justify-center items-center min-h-screen bg-gray-50 p-4 pl-0 dark:bg-[#18181B]">
+          <div className="bg-white shadow-lg rounded-2xl p-6 w-full min-h-full border dark:bg-[#1c1c20]">
+            <div className="flex h-16 shrink-0 items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                      <Link href="#">Configurações</Link>
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Conta</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+            <div className="flex flex-col items-start justify-center mt-8 mb-4 gap-4">
+              <h1 className="text-2xl font-bold text-center text-gray-900 dark:text-white">
+                Gerenciar Assinatura
+              </h1>
+              <Button onClick={handleManageSubscription}>
+                Gerenciar Assinatura
+              </Button>
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
